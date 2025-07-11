dd6b9e39cbdcab7da6c4b8a7a4109d5c950edc0d
Joel Berta <m8ypie@gmail.com>
2025-07-11 14:55:52 +1000

    fix client generation folder target

--- src/transform/mod.ts
+++ src/transform/mod.ts
@@ -1,7 +1,7 @@
 import { ApiClient } from "@m8ypie/open-api-http-client-generator";
 const apiClient = await ApiClient.generateFrom({
-  apiName: "inventory",
-  clientFilePath: "temp/",
+  apiName: "inventoryApiClient",
+  clientFilePath: "src/services/ebay/generatedClient/",
   url:
     "https://developer.ebay.com/api-docs/master/sell/inventory/openapi/3/sell_inventory_v1_oas3.json",
 });
