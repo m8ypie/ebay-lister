db3638cd91cfff4fd79874b0dfe42e2f91514391
Joel Berta <m8ypie@gmail.com>
2025-08-31 19:36:09 +1000

    Revert "ph"
    This reverts commit a95d7d35a760a8aa7de7c076b0f06988db2bb462.

--- src/transform/mod.ts
+++ src/transform/mod.ts
@@ -1,56 +1,8 @@
 import { ApiClient } from "@m8ypie/open-api-http-client-generator";
-import { camelCase } from "x/case";
-import { extractTypeFromExample } from "./openApiExampleToType.ts";
-import path from "node:path";
-
 const apiClient = await ApiClient.generateFrom({
-  apiName: "echo",
-  clientFilePath: "temp/",
-  filePath: "./openapi.json",
-  transformerOptions: {
-    methodNameExtractor: ({ methodJson, inUseMethodsNames }) => {
-      const colonSplitMethod = methodJson.summary.includes(":") &&
-          methodJson.summary.split(" ").length - 1 > 1
-        ? methodJson.summary.split(":")[1]
-        : methodJson.summary;
-
-      const entireDescFallback = methodJson.summary.replace(":", "");
-
-      const relevantName = inUseMethodsNames.includes(colonSplitMethod)
-        ? colonSplitMethod
-        : entireDescFallback;
-      return camelCase(relevantName);
-    },
-    requestBodyTransform: ({ methodJson, pathStr }) => {
-      const content = methodJson.requestBody.content;
-      const responseType = content["application/json"] || content["*/*"];
-      return extractTypeFromExample(
-        JSON.stringify(
-          responseType?.schema?.example || {},
-        ),
-        pathStr,
-      ).getBodyTransformProperty();
-    },
-    responseBodyTransform: ({ methodJson, pathStr }) => {
-      console.log(pathStr);
-      return extractTypeFromExample(
-        JSON.stringify(
-          methodJson.responses["200"]?.content["application/json"]?.example ||
-            {},
-        ),
-        pathStr,
-      ).getBodyTransformProperty();
-    },
-  },
+  apiName: "inventoryApiClient",
+  clientFilePath: "src/services/ebay/generatedClient/",
+  url:
+    "https://developer.ebay.com/api-docs/master/sell/inventory/openapi/3/sell_inventory_v1_oas3.json",
 });
-console.log("2");
 apiClient.writeApiClient();
-console.log("3");
-
-// const apiClient2 = await ApiClient.generateFrom({
-//   apiName: "buyDeal",
-//   clientFilePath: "temp/",
-//   url:
-//     "https://developer.ebay.com/api-docs/master/buy/deal/openapi/3/buy_deal_v1_oas3.json",
-// });
-// apiClient2.writeApiClient();
